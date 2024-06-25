a964f39c18071a8fdc1b5756537bf2e1d726ba84
Joshua Iluma <joshua@zestpayment.com>
2024-06-25 21:19:58 +0100

    Big O

---
+++ BigO/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        runLoop(10);
+
+    }
+    public static void runLoop(int n){
+        for(int i=0; i <n;i++){
+            System.out.println(i);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Data-Structures-and-Algorithms
 Data Structures and Algorithms in Java
+
+## Content
