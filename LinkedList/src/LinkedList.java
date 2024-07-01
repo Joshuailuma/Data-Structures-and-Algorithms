dccabce07107b87f7ce01b92475ec31c6ed9b785
Joshua Iluma <joshua@zestpayment.com>
2024-07-01 08:22:24 +0100

    Add more

---
+++ LinkedList/src/LinkedList.java
@@ -0,0 +1,199 @@
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node(int value){
+            this.value = value;
+        }
+    }
+    public LinkedList(int value){
+        Node newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this. length = 1;
+    }
+
+    public String getHead() {
+        if(head == null){
+            return "Head is "+ null;
+        } else {
+            return "Head is "+head.value;
+        }
+
+    }
+
+    public String getTail() {
+        if(tail == null){
+            return "Tail is "+ null;
+        } else {
+            return "Tail is "+tail.value;
+        }
+    }
+
+    public int getLength() {
+        return length;
+    }
+    public void printList(){
+       Node temp = head;
+       while (temp != null){
+           System.out.println(temp.value);
+           temp = temp.next;
+       }
+    }
+
+    public void append(int value){
+        Node node = new Node(value);
+        if (length == 0){
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+        length ++;
+    }
+
+    public Node removeLast(){
+        Node temp = head;
+        Node theNext = temp.next;
+        if(length == 1 || length == 0){
+            head = null;
+            tail = null;
+            return null;
+        } else {
+            while (theNext.next != null){
+                temp = theNext;
+                theNext = theNext.next;
+
+            }
+            tail = temp;
+            tail.next = null;
+            length --;
+            return theNext;
+        }
+    }
+
+    public Node prepend(int value){
+        Node newNode = new Node(value);
+        if(length ==0){
+            head = newNode;
+            tail = newNode;
+            length++;
+            return head;
+        }else {
+            newNode.next = head;
+            head = newNode;
+            length++;
+            return head;
+        }
+    }
+
+    public Node removeFirst(){
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length --;
+        if (length == 0){
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node get(int index){
+        Node temp = head;
+        if (index < 0 || index >= length) return null;
+        for (int i =0; i < length; i++){
+            if(i ==0){
+                temp = head;
+            }
+
+            if (i == index){
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return  temp;
+    }
+
+    // Or use this. And seems to be better
+    public Node gett(int index){
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        for (int i =0; i < index; i++){
+            temp = temp.next;
+        }
+        return  temp;
+    }
+
+    public boolean set(int index, int value){
+        Node temp = gett(index);
+        if(temp !=null){
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value){
+        if(index <0 || index > length) return false;
+        if(index == 0) {
+            prepend(value);
+            return true;
+        }
+
+        if(index == length-1){
+            append(value);
+            return true;
+        }
+
+            Node temp = gett(index-1);
+            Node newNode =  new Node(value);
+            newNode.next = temp.next;
+        temp.next = newNode;
+            length++;
+        return true;
+    }
+    public Node remove(int index){
+        if(index < 0 || index > length) return null;
+        if(index == 0) return removeFirst();
+
+        if (index == length) return removeLast();
+        Node prev = gett(index-1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        temp.next = null;
+        length --;
+        return temp;
+    }
+    public void reverse(){
+        Node before = null;
+        Node temp = head;
+        Node after = null;
+        head = tail;
+        tail = temp;
+
+        for (int i = 0; i < length; i++){
+            after = temp.next;
+            temp.next = before;
+            before= temp;
+            temp = after;
+        }
+    }
+
+    public Node findMiddleNode(){
+        Node fast = head;
+        Node slow =head;
+
+        while (fast!= null &&fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+
+    }
+}
---
+++ LinkedList/src/Main.java
@@ -0,0 +1,170 @@
+public class Main {
+    /**
+     * Please uncomment the titles you want to try
+    * */
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(0);
+
+        //=========APPEND VALUES============
+//        System.out.println(linkedList.getTail());
+//        linkedList.append(2); // Append a value
+        linkedList.append(1);
+        linkedList.append(2);
+        linkedList.append(3);
+        linkedList.append(4);
+        linkedList.append(5);
+
+
+        //========= REMOVE LAST============
+
+//        linkedList.printList();
+//        System.out.println("Removing last");
+//        linkedList.removeLast();
+//        linkedList.printList();
+//        System.out.println(linkedList.getLength());
+//        System.out.println("Done");
+
+
+        //======== PREPEND VALUES ===============
+//        linkedList.printList();
+//        System.out.println("Prepending values");
+//
+//        linkedList.prepend(5);
+//        linkedList.printList();
+
+
+
+        //======== REMOVE FIRST ===============
+//        linkedList.printList();
+//        System.out.println("Removing first");
+//        linkedList.removeFirst();
+
+        //======== GET AT INDEX ===============
+//        linkedList.printList();
+//        System.out.println("Getting at index");
+//        System.out.println(linkedList.gett(0).value);
+
+
+
+        //======== SET AT INDEX ===============
+//        linkedList.printList();
+//        System.out.println("Setting at index");
+//        System.out.println(linkedList.set(0,0));
+//        linkedList.printList();
+
+
+
+        //======== Add AT INDEX ===============
+//        linkedList.printList();
+//        System.out.println("Inserting at index");
+//        linkedList.insert(1,9);
+//        linkedList.printList();
+
+        //======== Remove AT INDEX ===============
+//        linkedList.printList();
+//        System.out.println("Removing at index");
+//        System.out.println("Removed "+linkedList.remove(2).value);
+//        linkedList.printList();
+
+
+
+        //======== REVERSE ===============
+//        linkedList.printList();
+//        System.out.println("Reversing");
+//        linkedList.reverse();
+//        linkedList.printList();
+
+
+
+        //======== FIND MIDDLE NODE===============
+      /**
+       * mplement a method called findMiddleNode that returns the middle node of the linked list.
+       *
+       * If the list has an even number of nodes, the method should return the second middle node.
+       *
+       *
+       * Method signature:
+       *
+       * public Node findMiddleNode()
+       *
+       *
+       *
+       *
+       * Example:
+       *
+       * LinkedList myList = new LinkedList(1);
+       * myList.append(2);
+       * myList.append(3);
+       * myList.append(4);
+       * myList.append(5);
+       * Node middleNode = myList.findMiddleNode();
+       * System.out.println(middleNode.value); // Output: 3
+       *
+       * myList.append(6);
+       * middleNode = myList.findMiddleNode();
+       * System.out.println(middleNode.value); // Output: 4
+       *
+       *
+       * When the linked list has an odd number of nodes, like 1 -> 2 -> 3 -> 4 -> 5, the function will find the exact middle node. In this case, it will return the node containing the value 3.
+       *
+       * When the linked list has an even number of nodes, there will be two middle nodes. The findMiddleNode function will return the second of these two middle nodes.
+       *
+       * For example, if the linked list is 1 -> 2 -> 3 -> 4 -> 5 -> 6, the two middle nodes are 3 and 4. The function will return the node containing the value 4.
+       *
+       *
+       *
+       * Note:
+       *
+       * In this problem, you should use the slow and fast pointer technique (also known as Floyd's Tortoise and Hare algorithm) to find the middle element of the linked list efficiently.
+       *
+       * The key idea is to have two pointers, one that moves twice as fast as the other. By the time the fast pointer reaches the end of the linked list, the slow pointer will be at the middle.
+       *
+       *
+       *
+       * DO NOT use the length attribute in your solution.
+       * */
+//        linkedList.printList();
+//        System.out.println("Getting middle");
+//        System.out.println("The middle node is "+linkedList.findMiddleNode().value);
+
+
+
+        //======== HAS A LOOP ===============
+        /**
+         * Write a method called hasLoop that is part of the linked list class.
+         *
+         * The method should be able to detect if there is a cycle or loop present in the linked list.
+         *
+         * The method should utilize Floyd's cycle-finding algorithm, also known as the "tortoise and hare" algorithm, to determine the presence of a loop efficiently.
+         *
+         * The method should follow these guidelines:
+         *
+         *
+         *
+         * Create two pointers, slow and fast, both initially pointing to the head of the linked list.
+         *
+         * Traverse the list with the slow pointer moving one step at a time, while the fast pointer moves two steps at a time.
+         *
+         * If there is a loop in the list, the fast pointer will eventually meet the slow pointer. If this occurs, the method should return true.
+         *
+         * If the fast pointer reaches the end of the list or encounters a null value, it means there is no loop in the list. In this case, the method should return false.
+         *
+         *
+         *
+         * Output:
+         *
+         * Return true if the linked list has a loop.
+         *
+         * Return false if the linked list does not have a loop.
+         *
+         *
+         *
+         * Constraints:
+         *
+         * You are not allowed to use any additional data structures (such as arrays) or modify the existing data structure.
+         *
+         * You can only traverse the linked list once.
+         * */
+
+    }
+}
